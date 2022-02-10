2b243b5daced3f66d221adc1bf974b50af6f4381
hidoshino <91507697+FilipePaiva10@users.noreply.github.com>
2022-02-10 12:03:59 -0300

    Update Erros

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const server = "https://api.thingspeak.com";
 
+const labelRange = window.document.getElementById("val_range");
+const range = window.document.getElementById("range");
+
 const keysAPI = {
     write: "SGUQ7K6F7GYGOPAQ",
     read: "XC2I9Y61ICW0VMV5",
     field: "ligar" // Campo para acender lâmpada
 }
 
-const sendAPI = async (value) => {
-    const http = new XMLHttpRequest();
-    const url = `${server}/update?api_key=${keysAPI.write}&${keysAPI.field}=${value}`;
+range.addEventListener('input', e => updateValue(e));
 
+
+const updateValue = (e) => {
+    labelRange.innerHTML = `Potência 💥: ${e.target.value} W`;
+}
+
+const sendAPI = async () => {
+    const http = new XMLHttpRequest();
+    const url = `${server}/update?api_key=${keysAPI.write}&field1=${range.value}`;
     http.open("GET", url);
     http.send();
 
-    http.onload = console.log(http.responseText + " " + value);
+    http.onload = console.log(http.responseText + " " + range.value);
+}
+
+window.onload = function () {
+    setInterval(sendAPI, 500)
 }
